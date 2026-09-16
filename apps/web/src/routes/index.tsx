7999270d95429b76fc5b5fce3065e813f55da722
Nail Abdullin <nail.abdullin@kuper.ru>
2026-09-17 01:26:52 +0300

    feat(web): add responsive typography components

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Menu } from "../modules/menu";
+import { Heading } from "../shared/components/heading";
+import { Text } from "../shared/components/text";
 
 function HomePage() {
   return (
-    <main className="relative min-h-dvh bg-black">
+    <main className="grid min-h-dvh place-items-center bg-black">
+      <div className="flex w-full flex-col items-center px-6 text-center">
+        <Heading
+          as="h1"
+          size="md"
+          className="max-w-3xl text-white"
+        >
+          Подбор роботизированных решений
+        </Heading>
+        <Text size="md" className="mt-6 max-w-xl text-white/70">
+          Сравнивайте роботизированные решения по задачам, характеристикам и
+          условиям внедрения.
+        </Text>
+      </div>
       <Menu />
     </main>
   );
